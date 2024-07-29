212c7a0b7c31c00958c833f5f7f89a0836e4fe4a
Rajeev Dalavi <rajeev.dalavi@gmail.com>
2024-07-29 22:02:18 +0530

    Initial commit

---
+++ src/main/java/com/neurospark/nerdnudge/contentmgr/NerdnudgeContentmgrApplication.java
@@ -0,0 +1,11 @@
+package com.neurospark.nerdnudge.contentmgr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NerdnudgeContentmgrApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(NerdnudgeContentmgrApplication.class, args);
+	}
+}
---
+++ src/test/java/com/neurospark/nerdnudge/contentmgr/NerdnudgeContentmgrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neurospark.nerdnudge.contentmgr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NerdnudgeContentmgrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
